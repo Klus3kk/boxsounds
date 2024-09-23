0f27a0b2c5c9b37d12c17a61943868ec34ffd09e
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-09-23 20:16:56 +0200

    Made reviews.

--- boxsounds/src/app/page.tsx
+++ boxsounds/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Vinyl from "@/components/Vinyl";
 import { Check, Star } from "lucide-react";
@@ -93,6 +94,83 @@ export default function Home() {
             </div>
         </div>
       </MaxWidthWrapper>
+    </section>
+
+    {/* value proposition section */}
+    <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+            <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                What our <span className="relative px-2">customers <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-red-500"/></span> say
+              </h2>
+              <img src='/snake-2.png' className="w-24 order-0 lg:order-2"/>
+            </div>
+
+            <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+              <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+                <div className="flex gap-0.5 mb-2">
+                  <Star className="size-5 text-red-600 fill-red-600"/>
+                  <Star className="size-5 text-red-600 fill-red-600"/>
+                  <Star className="size-5 text-red-600 fill-red-600"/>
+                  <Star className="size-5 text-red-600 fill-red-600"/>
+                  <Star className="size-5 text-red-600 fill-red-600"/>
+                </div>
+                <div className="text-lg leading-8">
+                  <p>
+                    "The vinyl box feels durable and I even got a compliment on the design. Had the case for two and a half months now 
+                    and <span className="p-0.5 bg-slate-800 text-white">the image is super clear</span>, on the box I had before, 
+                    the image started fading into green-ish color after a couple of weeks. Love it!"
+                  </p>
+                </div>
+                <div className="flex gap-4 mt-2">
+                  <img 
+                    className="rounded-full size-12 object-cover"
+                    src="/users/user-1.png"
+                    alt="user"
+                  />
+                  <div className="flex flex-col">
+                    <p className="font-semibold">Jonathan</p>
+                    <div className="flex gap-1.5 items-center text-zinc-600">
+                      <Check className="size-4 stroke-[3px] text-red-600"/>
+                      <p className="text-sm">Verified Purchase</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+              {/* second user review */}
+              <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+                <div className="flex gap-0.5 mb-2">
+                  <Star className="size-5 text-red-600 fill-red-600"/>
+                  <Star className="size-5 text-red-600 fill-red-600"/>
+                  <Star className="size-5 text-red-600 fill-red-600"/>
+                  <Star className="size-5 text-red-600 fill-red-600"/>
+                  <Star className="size-5 text-red-600 fill-red-600"/>
+                </div>
+                <div className="text-lg leading-8">
+                  <p>
+                  "I've had this vinyl box for two and a half months now, and it still feels incredibly durable. I even got a compliment on the design! The image quality is fantastic – 
+                  <span className='p-0.5 bg-slate-800 text-white'>super clear</span> and vibrant. On the box I had before, the image started fading 
+                  into a greenish color after just a few weeks, but this one is holding up beautifully."
+                  </p>
+                </div>
+                <div className="flex gap-4 mt-2">
+                  <img 
+                    className="rounded-full size-12 object-cover"
+                    src="/users/user-2.png"
+                    alt="user"
+                  />
+                  <div className="flex flex-col">
+                    <p className="font-semibold">Anna</p>
+                    <div className="flex gap-1.5 items-center text-zinc-600">
+                      <Check className="size-4 stroke-[3px] text-red-600"/>
+                      <p className="text-sm">Verified Purchase</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </MaxWidthWrapper>
     </section>
   </div>
   );
